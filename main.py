3fddd8e6cb61333440160bbbebdf2df6fafd7899
Sami Wolf <samiwolf@Samis-MacBook-Air.local>
2025-10-22 13:21:22 -0400

    Nothing burger changes

--- main.py
+++ main.py
@@ -309,10 +309,13 @@ def run_3_1(train_dataset,test_dataset):
 	model = make_cnn_model_part3_1()
 	optimizer = torch.optim.Adam(model.parameters(), lr=0.001, betas=(0.9,0.999))
 	train_dataloader,test_dataloader = construct_dataloaders(train_dataset,test_dataset,100)
+	start_time = _sync_and_retrieve_time()
 	stats = train(train_dataloader,test_dataloader,model,loss_fn,optimizer,epochs=10)
+	wall_time = _sync_and_retrieve_time() - start_time
 	with open("stats_3_1.pkl","wb") as f:
 		pickle.dump(stats,f)
 	print("test accuracy: " + str(stats[3][-1]))
+	print(f"training wall time: {wall_time}s")
 
 
 def plot_graphs():
